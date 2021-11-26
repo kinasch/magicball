588963e15baf31ff7fc68f4435f19a0d1e4e56f4
nico-mania <55563178+nico-mania@users.noreply.github.com>
2021-11-26 17:13:46 +0100

    Changed Playermovement from transform.position to velocity Added no movement while falling Changed filter of first idle_l sprite to point (no filter) with no compression

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform Player;
-    public float cameraZoom = -150f;
+    public float cameraZ = -150f;
 
 
     void Update()
     {
-        this.transform.position = new Vector3(Player.position.x, Player.position.y, cameraZoom);
+        this.transform.position = new Vector3(Player.position.x, Player.position.y, cameraZ);
 
 
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float moveX;
+    private Rigidbody2D playerRB;
+    private float playerYVelocityMax = -10;
     public float moveSpeed = 12f;
     public Animator animator;
+    
+    
 
-    void Update()
+    void FixedUpdate()
 
     {
-        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        if (playerRB.velocity.y >= 0)
+        {
+            animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
 
-        moveX = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(moveX, 0, 0) * Time.deltaTime * moveSpeed;
+            moveX = Input.GetAxisRaw("Horizontal");
+            playerRB.velocity = new Vector2(moveX * moveSpeed, 0);
+            Debug.Log(playerRB.velocity.y);
+        }
+        else
+        {
+            playerRB.velocity = new Vector2((moveX/2)*moveSpeed, Mathf.Clamp(playerRB.velocity.y, playerYVelocityMax, 0));
+        }
+    }
+
+    private void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody2D>();
     }
 }
