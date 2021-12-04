9489ca21de8d809179bed9a2171a2477223881f6
kinasch <janik@schoenberg.name>
2021-12-04 18:13:23 +0100

    19-speichern:
    Added Reading from Save

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,16 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
     [SerializeField] private string fileName = "leaderboard.sav";
+    [SerializeField] private Text testText;
 
     private float[] times;
+    private LeaderboardData lbData;
 
     // Start is called before the first frame update
     void Start()
     {
+        lbData = new LeaderboardData();
         // TestSave();
+        if (FileManager.FileExists(fileName))
+        {
+            lbData.FromJSON(FileManager.LoadFromFile(fileName));
+        }
+
+        testText.text = "";
+        if (lbData.lbentries != null)
+        {
+            foreach (var lbE in lbData.lbentries)
+            {
+                testText.text += lbE.name + ": " + Timer.FormatTime(lbE.time) +"\n";
+            }
+        }
     }
 
     public void TestSave()
